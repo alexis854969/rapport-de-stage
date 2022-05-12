941a4968690b46a8585a82a114229e8345c7f8a9
alexis854969 <a-auriou@stemarieduport.fr>
2022-05-12 15:16:47 +0200

    intiatialisation du projet

---
+++ accueil.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+<?php include('nav.php'); 
+    ?>
+          <article>
+        bablalalalalalalallalalalalalaalalalalalal <Bouton>
+    </article>
+    <article>
+         blalalalalalalalalalalalalalalalalala
+    </article>
+    <article>
+    <p>blablalalalalalalalalalaal<p>
+        <img src="">
+    </article>  
+</body>
+</html>
