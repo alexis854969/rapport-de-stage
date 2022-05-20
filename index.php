2aa8108d74e174fe733829004d6dfde301489584
alexis854969 <a-auriou@stemarieduport.fr>
2022-05-20 16:08:09 +0200

    Dev

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de recettes - Formulaire de Contact</title>
+    <link rel="stylesheet" href="style10.css" />
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
         rel="stylesheet"
