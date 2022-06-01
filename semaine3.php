b35a7c03463d91a31c6310a3013d1f041721efb4
alexis854969 <a-auriou@stemarieduport.fr>
2022-06-01 16:55:36 +0200

    f

--- semaine3.php
+++ semaine3.php
@@ -30,6 +30,63 @@
             <h3>titre du paragrahpe</h3>
             <p>ici Description projet</p>
         </fieldset>
-    </article>      
+    </article>   
+    <article>
+    <fieldset>
+        <h3>Lundi</h3>
+        <p> Ce matin, j’ai vu la conception de la mise en page de mon site. J’ai commencé l’aspect visuel. J’ai rempli le site avec mon rapport de stage. J’ai peaufiné le visuel et le contenu. Pour le moment, le site est rempli de la semaine 1 et 2. Je vais aussi devoir m’informer sur MYSQL. Pour la suite de mon site, cet après-midi. Je dois aller avec mon tuteur à la Roche Sur Yon. Pour récupérer un serveur au data center de INNLOG.
+Nous avons été cette après-midi avec Anthony, au data center à la Roche sur Yon. Pour une découverte du Data-center et remplacer les disquettes.
+La découverte : INNLOG stocke leur domaine dans un DATA Center. Anthony m’a expliqué tous les appareils du data center et leur fonction. J’ai pris des photos du matériel. Les domaines des utilisateurs de chez INNLOG sont stockés et reliés avec la fibre, il y la maitrise des appareils en double, s’il y a une panne, les machines secondaires prennent le relais en attendant la réparation des machines principales.
+Les tâches que j’ai du réalisé. Il y a un appareil qui sauvegarde les domaines et il faut changer toutes les semaines, les cartouches qui sont sauvegardées et mettre des cartouches vides. C’est ce que j’ai fait. On a fait cela pour toutes les cartouches. Anthony doit s’occuper de cette tâche toute la semaine car les cartouches sont pleines. Grâce à cette tâche, j’ai découvert comment fonctionne les serveurs dans un data-center et toute la partie connectique.
+</p>
+    </fieldset>
+</article>     
+    <article>
+    <fieldset>
+        <h3>mardi</h3>
+        <p>Je me suis occupé ce matin d’Hyper U avec Anthony. On a mis en place un serveur avec un nas pour le faire fonctionner avec un appareil virtuel, mais cela ne s’est pas passé comme prévu. Les serveurs ne fonctionnent pas. Nous avons testé toute la journée. On a donc fait un peut de réseau avec Anthony. Nous n’ avons pas eu le temps de finir les tâches à cause de l’AS ne convenait pas au serveur.
+</p>
+    </fieldset>
+</article>
+<article>
+    <fieldset>
+        <h3>Mercredi</h3>
+        <p>
+</p>Journée du citoyen
+
+    </fieldset>
+</article>   
+<article>
+    <fieldset>
+        <h3>Jeudi</h3>
+        <p>J’ai continué ce matin la formation open classroom MY SQL et PHP, afin d’améliorer mes compétences. Voici, les cours que j’ai vu.
+    • Ecoutez la requête d’utilisation grâce au url
+    • Administrez des formulaires de façon sécurisé
+    • Activez le partage des fichiers
+    • Implémentez un système de connexion
+    • Conservez des données grâce au session et au cookies
+    • Travaillez une base de données
+    • Mettre en place une base de données avec PHP admin
+    • Accédez aux données en PHP avec HBA
+
+Puis on a réussi à faire fonctionner le serveur. On a passé du temps, a le réalisé car on a trouvé le problème.
+
+L’après-midi
+    • Ajoutez, modifiez et supprimez des recettes
+    • Ajoutez des commentaires grâce au jointure SQL.
+Yannick Moreau m’a demandé de regarder les lignes de code que j’ai réalisé et comparé avec ce qu’on a fait Yannick pour comprendre ce que j’ai fait et peaufiner mon langage PHP.
+
+</p>
+    </fieldset>
+</article>   
+<article>
+    <fieldset>
+        <h3>Vendredi</h3>
+        <p>Ce matin, j’ai travaillé avec GITHUB. J’ai regardé des tutoriels que mon maitre de stages m’a conseillé. Pour apprendre les bases, c’est mieux de comprendre le logiciel. J’ai regardé 2 tutoriels en attendant Yannick Moreau car il était en réunion. Ensuite, j’ai du améliorer mon site avec du C55, j’ai regardé un tutoriel qui m’a aidé. J’ai avancé sur mes tâches.
+L’après-midi, avancement du du C55, on a corrigé les erreurs de C55 et nous avons peaufiné. On a amélioré l’aspect graphique. Cet après-midi, j’ai fait des corrections de bug (tout ce que j’ai produit sur GIT.HUB)
+
+</p>
+    </fieldset>
+</article>      
 </body>
 </html>
