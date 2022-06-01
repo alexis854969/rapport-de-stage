034777b61176e4565e009b588b8db9127cede932
alexis854969 <a-auriou@stemarieduport.fr>
2022-06-01 17:04:12 +0200

    s

--- semaine4.php
+++ semaine4.php
@@ -30,6 +30,44 @@
             <h3>titre du paragrahpe</h3>
             <p>ici Description projet</p>
         </fieldset>
-    </article>      
+    </article>  
+        <article>
+           <fieldset>
+                <h3>lundi </h3>
+                <p> Ce matin, j’ai réalisé de l’Hyper U sur les serveurs, data-center.
+J’ai paramétré un OS W10 et windows serveur 2022.
+J’ai fait beaucoup de virtualisation l’après-midi. 
+Je devrais m’occuper de l’OS CABINTU sur hyper u
+
+Cette après-midi, j’ai crée sur SWITCH VIRTUEL, regroupe un pfessense, ubentu, windows et windows serveur 2022, avec Hyper U. Nous avons rendu windows serveur pour tous relier au pfsense qui est paramétré en virtualisation. Cela va servir pour le site internet soit joignable de partout</p>
+            </fieldset>
+        </article>
+        <article>
+           <fieldset>
+                <h3>mardi </h3>
+                <p>J’ai continué le serveur mais beaucoup de problèmes avec UBENTU. Ce matin, nous avons réglé tous les problèmes et j’ai aidé à paramétrer des antennes wifi remplies de problèmes.
+L’après-midi, nous avons essayé les problèmes d’UBENTU mais je n’ai pas réussi à le résoudre. J’ai aidé Thierry a faire de la livraison de cable toute la fin de l’après-midi et l’aidé sur le support. </p>
+            </fieldset>
+        </article>
+        <article>
+           <fieldset>
+                <h3>Mercredi </h3>
+                <p>Ce matin, je suis en observation sur le support logiciel (wms) Antoine m’a expliqué les tâches qu’ils doivent réaliser. Ils aident les clients qui ont des problèmes avec le logiciel. Je l’ai observé réaliser des tâches et des tickets, toute la journée. Il m’a expliqué ses fonctions et les caractéristiques, il m’a donné un moment pour tous m’expliquer.
+L’après-midi, j’ai observé le travail qu’il réalise. Antoine m’a donné une feuille avec toutes les explications. Cette journée d’observation m’a aidé à améliorer mes connaissances. J’ai assisté à du dépannage et du débogage. J’ai aussi mieux compris le logiciel testfry.  </p>
+            </fieldset>
+        </article>
+        <article>
+           <fieldset>
+                <h3>Jeudi </h3>
+                <p> ferrier</p>
+            </fieldset>
+        </article>        
+        <article>
+           <fieldset>
+                <h3>Vendredi </h3>
+                <p>ferrier </p>
+            </fieldset>
+        </article>                        
+                  
 </body>
 </html>
