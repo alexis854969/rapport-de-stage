aca381ee573c2beeb85d87885cc8999e26cab162
alexis854969 <a-auriou@stemarieduport.fr>
2022-06-01 16:44:38 +0200

    f

--- accueil.php
+++ accueil.php
@@ -1,55 +1,44 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
+<head>
     <meta charset="utf-8" />
      <link rel="stylesheet" href="style10.css"/>
       <!-- ne pas oublier de changerle nom de la feuille de style-->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-  </head>
-
-<body>
-    <?php include('nav.php'); ?>
-        <article>
-           <fieldset>
-                <h3>Dérnière journée</h3>
-                <?php 
-        include 'read.php'
-        ?>
-    <table>
-   <thead>
-     <tr>
-       <th>Semaine</th>
-       <th>Journée</th>
-       <th>Détail</th>
-     </tr>
-   </thead>
-   <tbody>
-     <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
-     <tr>
-       <td><?php echo htmlspecialchars($row['semaine']); ?></td>
-       <td><?php echo htmlspecialchars($row['journee']); ?></td>
-       <td><?php echo htmlspecialchars($row['detail']); ?></td>
-     </tr>
-     <?php endwhile; ?>
-   </tbody>
-    </table>
-            <a href="ajout.php"><button>Ajouter</button></a>
-                </fieldset>
-        </article>
-        <article>
-           <fieldset>
-                <h3>Description du projet</h3>
-                <p>ici Description projet</p>
+    <title>Accueil</title> 
+</head> 
+ <body>
+   <?php include('nav.php'); ?>
+   <?php  include 'read.php'; ?>
+      <article>
+          <fieldset id="accueilFieldset">
+             <h3>Description du projet</h3>
+             <p>Un sites web qui permet de noter,lire,crée ses journée de stage. grace a un script qui note les journée sur une pages semaine</p>
+             <p>Les technologie utiliser sont :</p>
+             <ul id="lstTechno"> 
+              <li> front-end : css3/html5  </li>
+              <li> backend : php7   </li>
+              <li> bdd : MySql   </li>
+            </ul>
+             <div id="navDescription">
+              <a href="semaine1.php" ><button class="btnSemaine">semaine 1</button></a>
+              <a href="semaine2.php" ><button class="btnSemaine">semaine 2</button></a>
+              <a href="semaine3.php" ><button class="btnSemaine">semaine 3</button></a>
+              <a href="semaine4.php" ><button class="btnSemaine">semaine 4</button></a>
+              <a href="semaine5.php" ><button class="btnSemaine">semaine 5</button></a>
+              <a href="ajout.php"><button id="btnAjouter">Ajouter</button></a>
+            </div>
             </fieldset>
+           
         </article>        
         <article>
            <fieldset>
                 <h3>Presentation de l'étudiant</h3>
                 <p>Je m'appele alexis Auriou , j'ai 17 ans , éleves a saint-marie du port en premier SN en profesionel </p>
                 <img id="maphoto"src="" alt="ma photo">
-            </fieldset>
-        </article>    
-</body>
+            </fieldset>       
+        </article>  
+   </article>    
+  </body>
 </html>
--- ajout.php
+++ ajout.php
@@ -16,10 +16,10 @@
      <h1>Ajout</h1>
      <form action="insert.php" method="GET">
         <fieldset>
-            <label>Semaine</label><input type="number" name ="semaine" placeholder="indiquez 1 pour semaine 1, 2 pour semaine, 2 etc.">
-            <label>Jour</label><input type="text" name="journee" placeholder="Lundi, Mardi, Mercredi, Jeudi ou Vendredi">
-            <label>Détail</label><input type="text" name="detail" placeholder="détailler votre journée">
-            <input type="submit">
+            <label>Semaine</label><input class="formEnvoie" type="number" name ="semaine" placeholder="indiquez 1 pour semaine 1, 2 pour semaine, 2 etc.">
+            <label>Jour</label><input  class="formEnvoie"type="text" name="journee" placeholder="Lundi, Mardi, Mercredi, Jeudi ou Vendredi">
+            <label>Détail</label><input  class="formEnvoie" type="text" name="detail" placeholder="détailler votre journée">
+            <input type="submit" id="btnEnvoyer" class="formEnvoie">
         </fieldset>
     </form>
 </body>
